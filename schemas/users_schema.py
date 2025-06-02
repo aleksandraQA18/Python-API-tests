4f3ce6940850c70addf12c41df2df226b3a7c227
Ola18 <a.glebocka18@gmail.com>
2025-06-02 18:13:37 +0200

    Add file users_schema.py

---
+++ schemas/users_schema.py
@@ -0,0 +1,45 @@
+USERS_SCHEMA = [
+    {
+        "id": int,
+        "name": "str",
+        "username": "str",
+        "email": "str@april.biz",
+        "address": {
+            "street": "str",
+            "suite": "str",
+            "city": "str",
+            "zipcode": "str",
+            "geo": {"lat": "str", "lng": "str"},
+        },
+        "phone": "str",
+        "website": "str",
+        "company": {
+            "name": "str",
+            "catchPhrase": "str",
+            "bs": "str",
+        },
+    }
+]
+
+USER_SCHEMA = [
+    {
+        "id": int,
+        "name": "str",
+        "username": "str",
+        "email": "str@april.biz",
+        "address": {
+            "street": "str",
+            "suite": "str",
+            "city": "str",
+            "zipcode": "str",
+            "geo": {"lat": "str", "lng": "str"},
+        },
+        "phone": "str",
+        "website": "str",
+        "company": {
+            "name": "str",
+            "catchPhrase": "str",
+            "bs": "str",
+        },
+    }
+]
