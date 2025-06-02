82d87fd2986c3b8f70fc775db6610726263947c7
Ola18 <a.glebocka18@gmail.com>
2025-06-02 18:05:55 +0200

    Add post factory module for generating fake posts

---
+++ factory/post_factory.py
@@ -0,0 +1,16 @@
+from random import randint
+from faker import Faker
+import logging
+
+LOG = logging.getLogger("faker")
+LOG.setLevel(logging.INFO)
+
+fake = Faker()
+
+
+def generate_post():
+    return {
+        "title": fake.word(),
+        "body": fake.sentence(nb_words=10),
+        "userId": randint(1, 5),
+    }
