4c847ba2d31d35daf9c222eb65ad28e8f7c46a03
aleksandraQA18 <pythonpractice393@gmail.com>
2025-06-13 19:25:39 +0200

    add user schema

--- schemas/users_schema.py
+++ schemas/users_schema.py
@@ -1,45 +1,43 @@
 USERS_SCHEMA = [
     {
         "id": int,
-        "name": "str",
-        "username": "str",
-        "email": "str@april.biz",
+        "name": str,
+        "username": str,
+        "email": str,
         "address": {
-            "street": "str",
-            "suite": "str",
-            "city": "str",
-            "zipcode": "str",
-            "geo": {"lat": "str", "lng": "str"},
+            "street": str,
+            "suite": str,
+            "city": str,
+            "zipcode": str,
+            "geo": {"lat": str, "lng": str},
         },
-        "phone": "str",
-        "website": "str",
+        "phone": str,
+        "website": str,
         "company": {
-            "name": "str",
-            "catchPhrase": "str",
-            "bs": "str",
+            "name": str,
+            "catchPhrase": str,
+            "bs": str,
         },
     }
 ]
 
-USER_SCHEMA = [
-    {
-        "id": int,
-        "name": "str",
-        "username": "str",
-        "email": "str@april.biz",
-        "address": {
-            "street": "str",
-            "suite": "str",
-            "city": "str",
-            "zipcode": "str",
-            "geo": {"lat": "str", "lng": "str"},
-        },
-        "phone": "str",
-        "website": "str",
-        "company": {
-            "name": "str",
-            "catchPhrase": "str",
-            "bs": "str",
-        },
-    }
-]
+USER_SCHEMA = {
+    "id": int,
+    "name": str,
+    "username": str,
+    "email": str,
+    "address": {
+        "street": str,
+        "suite": str,
+        "city": str,
+        "zipcode": str,
+        "geo": {"lat": str, "lng": str},
+    },
+    "phone": str,
+    "website": str,
+    "company": {
+        "name": str,
+        "catchPhrase": str,
+        "bs": str,
+    },
+}
