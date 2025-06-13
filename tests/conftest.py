0212d065ddaaaddf45f9ba43849a4ccc33c9a906
aleksandraQA18 <pythonpractice393@gmail.com>
2025-06-13 19:26:08 +0200

    add user fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
-import pytest
-from factory.post_factory import generate_post
-from lib.posts import Posts
-from config import LOG
 import json
+
+import pytest
+
+from config import LOG
+from factory.post_factory import generate_post
+from factory.user_factory import generate_user
+from lib.posts import Posts
+from lib.users import Users
 
 
 @pytest.fixture
@@ -10,10 +14,24 @@ def random_product_id():
     return Posts().get_random_post_id()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def add_new_post():
     payload = json.dumps(generate_post())
     response = Posts().add_new_post(payload)
+    assert response.ok
+    LOG.debug(response.json())
+    return response.json()
+
+
+@pytest.fixture
+def random_user_id():
+    return Users().get_random_user_id()
+
+
+@pytest.fixture
+def add_new_user():
+    payload = json.dumps(generate_user())
+    response = Users().add_new_user(payload)
     assert response.ok
     LOG.debug(response.json())
     return response.json()
